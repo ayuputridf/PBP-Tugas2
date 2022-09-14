bb08a4ab97803fbc4bf69c392b1567afb3f8b2fa
Ayu Putri Dewi Fitriyani <ayuputridf2@.com>
2022-09-14 18:35:20 +0700

    Connect models with views and template (reference : soal Lab1)

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem #Catalog Item lihat di class models.py
+
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Ayu Putri Dewi Fitriyani',
+    'ID': '2106654845',
+    }
+    return render(request, "katalog.html", context)
